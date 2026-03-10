3681840c09fd5dc2d73eb6f3bf2538e233d55ea8
Diana Pratiwi <deedeepratiwi@gmail.com>
2026-03-10 22:27:16 +0700

    homework week 6

---
+++ 06-batch/homework/test_spark.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .master("local[*]") \
+    .appName('test') \
+    .getOrCreate()
+
+print(f"Spark version: {spark.version}")
+
+df = spark.range(10)
+df.show()
+
+spark.stop()
+
+
+# In[ ]:
+
+
+
+
