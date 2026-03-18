57ca96b3a5cd69728cd8fd4db62e4668eff5e4e6
Diana Pratiwi <91868779+deedeepratiwi@users.noreply.github.com>
2026-03-18 15:56:42 +0000

    homework week 7

---
+++ 07-streaming/src/job/session_window_job_hw.py
@@ -0,0 +1,85 @@
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import EnvironmentSettings, StreamTableEnvironment
+
+
+def create_events_source_kafka(t_env):
+    table_name = "events"
+    source_ddl = f"""
+        CREATE TABLE {table_name} (
+            lpep_pickup_datetime VARCHAR,
+            lpep_dropoff_datetime VARCHAR,
+            PULocationID INTEGER,
+            DOLocationID INTEGER,
+            passenger_count INTEGER,
+            trip_distance DOUBLE,
+            tip_amount DOUBLE,
+            total_amount DOUBLE,
+            event_timestamp AS TO_TIMESTAMP_LTZ(lpep_pickup_datetime, 'yyyy-MM-dd HH:mm:ss'),
+            WATERMARK for event_timestamp as event_timestamp - INTERVAL '5' SECOND
+        ) WITH (
+            'connector' = 'kafka',
+            'properties.bootstrap.servers' = 'redpanda:29092',
+            'topic' = 'green-trips',
+            'scan.startup.mode' = 'earliest-offset',
+            'properties.auto.offset.reset' = 'earliest',
+            'format' = 'json'
+        );
+        """
+    t_env.execute_sql(source_ddl)
+    return table_name
+
+
+def create_session_window_sink(t_env):
+    table_name = 'session_window_results'
+    sink_ddl = f"""
+        CREATE TABLE {table_name} (
+            window_start TIMESTAMP(3),
+            window_end TIMESTAMP(3),
+            PULocationID INT,
+            num_trips BIGINT,
+            PRIMARY KEY (window_start, window_end, PULocationID) NOT ENFORCED
+        ) WITH (
+            'connector' = 'jdbc',
+            'url' = 'jdbc:postgresql://postgres:5432/postgres',
+            'table-name' = '{table_name}',
+            'username' = 'postgres',
+            'password' = 'postgres',
+            'driver' = 'org.postgresql.Driver'
+        );
+        """
+    t_env.execute_sql(sink_ddl)
+    return table_name
+
+
+def session_window_aggregation():
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.enable_checkpointing(10 * 1000)
+    env.set_parallelism(1)  # green-trips topic has 1 partition
+
+    settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
+    t_env = StreamTableEnvironment.create(env, environment_settings=settings)
+
+    try:
+        source_table = create_events_source_kafka(t_env)
+        sink_table = create_session_window_sink(t_env)
+
+        t_env.execute_sql(f"""
+        INSERT INTO {sink_table}
+        SELECT
+            window_start,
+            window_end,
+            PULocationID,
+            COUNT(*) AS num_trips
+        FROM TABLE(
+            SESSION(TABLE {source_table}, DESCRIPTOR(event_timestamp), INTERVAL '5' MINUTE)
+        )
+        GROUP BY window_start, window_end, PULocationID;
+
+        """).wait()
+
+    except Exception as e:
+        print("Session window aggregation failed:", str(e))
+
+
+if __name__ == '__main__':
+    session_window_aggregation()
